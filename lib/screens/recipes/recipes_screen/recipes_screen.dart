0cc591c39d3edf5664642277caff22ed4c41e3b1
andreferrario <andreaantonio.ferrario@mail.polimi.it>
2024-05-12 15:14:00 +0200

    lib: add le mie ricette screen
    Signed-off-by: andreferrario <andreaantonio.ferrario@mail.polimi.it>

--- lib/screens/recipes/recipes_screen/recipes_screen.dart
+++ lib/screens/recipes/recipes_screen/recipes_screen.dart
@@ -1,5 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:condivisionericette/controller/PageController.dart';
 import 'package:condivisionericette/utils/constant.dart';
 import 'package:condivisionericette/widget/header.dart';
+import 'package:condivisionericette/widget/recipe_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -8,16 +11,60 @@ class RecipesScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const SafeArea(
-      child: SingleChildScrollView(
+    return Scaffold(
+      body: SingleChildScrollView(
         primary: false,
-        padding: EdgeInsets.all(defaultPadding),
+        padding: const EdgeInsets.all(defaultPadding),
         child: Column(
           children: [
-            Header(),
-            SizedBox(height: defaultPadding),
+            const Header(),
+            const SizedBox(height: defaultPadding),
+            SingleChildScrollView(
+              primary: false,
+              physics: const BouncingScrollPhysics(),
+              child: StreamBuilder(
+                stream: FirebaseFirestore.instance
+                    .collection("recipes")
+                    .snapshots(),
+                builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                  if (snapshot.hasError) {
+                    return const Center(
+                      child: Text("Something went wrong"),
+                    );
+                  }
+                  if (snapshot.data!.docs.isEmpty) {
+                    return const Center(
+                      child: Text("Nessuna ricetta trovata"),
+                    );
+                  }
+                  return Column(
+                    children:
+                        snapshot.data!.docs.map((DocumentSnapshot document) {
+                      return Padding(
+                          padding: const EdgeInsets.all(defaultPadding ),
+                          child: RecipeListItem(
+                              imageUrl: document["cover_image"],
+                              title: document["nome_piatto"],
+                              description: document["descrizione"],
+                              key: ValueKey(document.id)));
+                    }).toList(),
+                  );
+                },
+              ),
+            )
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          ref.read(pageControllerProvider).setPage(5);
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
